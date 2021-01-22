33438ce79e32d678b4a9a6723964d0679e6eb950
Ahmed Abbas <ahmed.a.mirza1@gmail.com>
2021-01-22 18:22:09 +0100

    Added scripts to reproduce bug in AMC

---
+++ build_amc_py.sh
@@ -0,0 +1,12 @@
+# Creating a python virtual environment: 
+python3 -m venv amc_env 
+
+# Activate the environment:
+source amc_env/bin/activate
+
+# Install dependencies before building LPMP:
+pip install wheel
+pip install numpy
+
+# Install LPMP python bindings:
+python3 -m pip install .
